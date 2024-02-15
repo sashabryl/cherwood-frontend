06698da19bdca4493cef2ab08398f52269845301
Akilover <119785153+liza-strykharchuk@users.noreply.github.com>
2024-02-15 18:07:24 +0200

    options

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,9 @@
 import { Cherwood } from "./helpers/Cherwood"; 
-import cherwoodData from "../src/data (4).json";
-
 import { Option } from "./helpers/Options";
-import options from "../src/options.json";
 import { UserType } from "./helpers/UserType";
 import { CartItem } from "./helpers/ChartInterface";
 import axios from "axios";
 import { BookingItem } from "./helpers/BookingInterface";
-
-function wait(delay: number) {
-  return new Promise(resolve => setTimeout(resolve, delay));
-}
 
 // export async function getCherwood(): Promise<Cherwood[]> {
 //   return wait(500)
@@ -138,13 +131,33 @@ export async function getBooking(access): Promise<BookingItem[]> {
 
 
 
+// export async function getOptions(): Promise<Option[]> {
+//   return wait(500)
+//     .then(() => {
+//       const jsonData2 = options as Option[];
+//       return Promise.resolve(jsonData2);
+//     });
+// }
+
 export async function getOptions(): Promise<Option[]> {
-  return wait(500)
-    .then(() => {
-      const jsonData2 = options as Option[];
-      return Promise.resolve(jsonData2);
+  const apiUrl = 'https://cherwood-backend-cold-river-1843-quiet-breeze-3861.fly.dev/api/categories/';
+
+  return fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data from ${apiUrl}`);
+      }
+      return response.json();
+    })
+    .then((jsonData: Option[]) => {
+      return Promise.resolve(jsonData);
+    })
+    .catch(error => {
+      console.error(error);
+      return Promise.reject(error);
     });
 }
+
 
 export const handleChart = async (currentAction: string, id: number) => {
   try {
